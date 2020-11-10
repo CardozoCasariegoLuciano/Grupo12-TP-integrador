679ba56eb57620f7fec6e6d6dd4483ae5f7eab77
Ramiro <Ramiro@DESKTOP-TVVON45>
2020-11-10 20:39:07 -0300

    g

--- src/ar/edu/unq/po2/tpintegrador/Celular.java
+++ src/ar/edu/unq/po2/tpintegrador/Celular.java
@@ -11,19 +11,26 @@ public class Celular {
 	public Integer getSaldo() {
 		return saldo;
 	}
+	
+	
 	public void setSaldo(int saldo) {
 		this.saldo = saldo;
 	}
+	
+	
 	public String getNumero() {
 		return numero;
 	}
+	
 	public void estacionar(String patente) {
 		//Envia mensaje a la app dentro del celular y es quien se encargaria de enviar mensaje al sistema
 		app.estacionar(patente);
-		
 	}
+	
+	
 	public void finDeEstacionamiento() {
 		// TODO Auto-generated method stub
+		
 		
 	}
 	
