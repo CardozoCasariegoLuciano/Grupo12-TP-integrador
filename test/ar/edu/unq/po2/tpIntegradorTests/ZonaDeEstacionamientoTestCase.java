4493ec4ef8d05d9f6ffb9c8af6a03c816515f55b
lucianoCardozo <lucianoc.c117@gmail.com>
2020-11-12 15:49:33 -0300

    creada la clase Ubicacion

--- test/ar/edu/unq/po2/tpIntegradorTests/ZonaDeEstacionamientoTestCase.java
+++ test/ar/edu/unq/po2/tpIntegradorTests/ZonaDeEstacionamientoTestCase.java
@@ -3,16 +3,23 @@ package ar.edu.unq.po2.tpIntegradorTests;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import ar.edu.unq.po2.tpintegrador.Inspector;
 import ar.edu.unq.po2.tpintegrador.PuntoDeVenta;
+import ar.edu.unq.po2.tpintegrador.Ubicacion;
 import ar.edu.unq.po2.tpintegrador.ZonaDeEstacionamiento;
 
 class ZonaDeEstacionamientoTestCase {
 
 	ZonaDeEstacionamiento unaZona;
+	Ubicacion unaUbicacion;
 	
 	Inspector unInspector;
 	
@@ -20,8 +27,9 @@ class ZonaDeEstacionamientoTestCase {
 	public void setUp() {
 		
 		unInspector = mock(Inspector.class);
+		unaUbicacion = mock(Ubicacion.class);
 		
-		unaZona = new ZonaDeEstacionamiento(unInspector);
+		unaZona = new ZonaDeEstacionamiento(unInspector, unaUbicacion);
 		
 	}
 	@Test
@@ -29,6 +37,7 @@ class ZonaDeEstacionamientoTestCase {
 		
 		assertEquals(unInspector, unaZona.getInspector());
 		assertEquals(0, unaZona.getPuntosDeVenta().size());
+		assertEquals(unaUbicacion, unaZona.getUbicacion());
 	}
 	
 	@Test
@@ -41,6 +50,18 @@ class ZonaDeEstacionamientoTestCase {
 		unaZona.abrirPuntoDeVenta(unPV);
 		
 		assertEquals(1, unaZona.getPuntosDeVenta().size());
+	}
+	
+	
+	@Test
+	void unaZonaPuedeObtenerTodosLosRegistrosDeCompra() {
+				
+		PuntoDeVenta puntoDeVenta = mock(PuntoDeVenta.class);		
+		unaZona.abrirPuntoDeVenta(puntoDeVenta);
+		
+		unaZona.registrarComprasDePuntosDeVenta();
+		
+		verify(puntoDeVenta, atLeastOnce()).getRegistrosDeCompra();
 	}
 
 }
